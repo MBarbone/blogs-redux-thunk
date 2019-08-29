e102c99f8a7def9f4a93d3d33f7171b3861972e3
Mike <michael.barbone@gmail.com>
2019-08-29 15:13:14 -0400

    Refactor memoize code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 import _ from "lodash";
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => {
   return async dispatch => {
     const response = await jsonPlaceHolder.get("/posts");
@@ -9,12 +15,10 @@ export const fetchPosts = () => {
   };
 };
 
-export const fetchUser = id => dispatch => {
-  _fetchUser(id, dispatch);
+export const fetchUser = id => {
+  return async dispatch => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+    dispatch({ type: "FETCH_USER", payload: response.data });
+  };
 };
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
-  const response = await jsonPlaceHolder.get(`/users/${id}`);
-
-  dispatch({ type: "FETCH_USER", payload: response.data });
-});
